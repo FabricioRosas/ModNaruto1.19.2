2e260b6971c7ddb2c98f2212bd58200fce54ab54
FabricioRosas11 <fabricio.rosas@unmsm.edu.pe>
2022-12-04 17:36:14 -0500

    Probando commit en intellij

--- src/main/java/net/megambit/narutomod/NarutoMod.java
+++ src/main/java/net/megambit/narutomod/NarutoMod.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(NarutoMod.MOD_ID)
+//porbando commit
 public class NarutoMod
 {
     // Define mod id in a common place for everything to reference
